624ef6aeca1eec6f820288e599b785084736ce74
Stuart Sackler <stuart@sackler.ca>
2019-03-26 23:55:57 -0400

    initial upload

---
+++ potent.js
@@ -0,0 +1,29 @@
+//red 5v
+//black ground
+//grey a1
+
+var five = require("johnny-five"),
+  board, potentiometer;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create a new `potentiometer` hardware instance.
+  potentiometer = new five.Sensor({
+    pin: "A1",
+    freq: 250
+  });
+
+  // Inject the `sensor` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    pot: potentiometer
+  });
+
+  // "data" get the current reading from the potentiometer
+  potentiometer.on("data", function() {
+    console.log(this.value, this.raw);
+  });
+});
